da60ab4301b2275abd3625dceedeb708706ffa18
imanewkm <imanewkm02@gmail.com>
2024-10-15 23:52:30 +0100

    .\SearchForm.css

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import "./SearchForm.css";
 
 const SearchForm = () => {
   return (
-    <div>SearchForm</div>
+    <div className='search-form'>
+      <div className='search-form-content'>
+        <form className='search-form'>
+          <div className='search-form-elem flex flex-sb bg-white'>
+            <input type = "text"
+            className='form-control' placeholder='The Lost World ...' />
+            <button type = "submit" className='flex flex-c'>
+              <FaSearch className='text-green' size = {32} />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
